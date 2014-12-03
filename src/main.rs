3acada63116d6a1a3307547d563d93d2bd35a64c
Alan Andrade <alan.andradec@gmail.com>
2014-12-03 00:16:21 -0800

    use files as buffers

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,48 @@
 
 use std::io::process::Command;
 use manifest::Manifest;
-use std::io::net::pipe::UnixStream;
 use std::io::File;
 
 mod manifest;
 
 fn main () {
-    // First runs spidermonkey parser
-    let source_code = ["Reflect.parse(", ");"];
-
     let mut manifest = Manifest::new();
 
     // Might need to escape quotation marks here.
     let content  = match manifest.read_to_string() {
-        Ok(c) => c,
+        Ok(c) => {
+            let mut bytes = c.into_bytes();
+            bytes.retain(|&c| c != 10); // Remove LF
+            String::from_utf8(bytes).unwrap()
+        }
         Err(e) => panic!(e)
     };
-
-    println!("js gotten: {}", content);
+    println!("js from manifest: {}", content);
 
     let source_code =
-        format!(
-            "'print(JSON.stringify(Reflect.parse(\"{}\")))'",
-            content);
+        format!("print(JSON.stringify(Reflect.parse(\"{}\")))\n", content);
 
     println!("Source code: \n{}", source_code);
+    let source_code_path = Path::new(".oxidize-source");
+    let mut source_code_file = File::create(&source_code_path);
+    source_code_file.write(source_code.as_bytes()).ok();
 
-    let stdout = Command::new("uname").arg("-v").output().ok().unwrap();
+    let ast_path = Path::new(".oxidize-out");
+    match Command::new("js").
+            env("JS_STDOUT", ".oxidize-out").
+            env("JS_STDERR", ".oxidize-error").
+            arg("-f").
+            arg(source_code_path).
+            status() {
+                Ok(s) => println!("{}", s),
+                Err(e) => panic!(e)
+            }
 
-    println!("uname: {}", String::from_utf8_lossy(stdout.output.as_slice()))
+    let result =
+        match File::open(&ast_path) {
+            Ok(mut f) => f.read_to_string().ok().unwrap(),
+            Err(e) => panic!("{}", e)
+        };
 
-    //let file_path = Path::new("~/.oxidize.out");
-    //let output_file = File::create(&file_path);
-
-    //let command = Command::new("js").
-        //arg("-e").
-        //arg(source_code.as_slice()).
-        //env("JS_STDOUT", file_path.clone()).
-        //spawn().
-        //ok().
-        //unwrap();
-
-    //let mut file = File::open(&file_path);
-    //let ast = file.read_to_string().ok().unwrap();
-
-    //println!("ast: {}", ast);
-
-    //match Command::new("js").args(&["-e", source_code.as_slice()]).output() {
-        //Ok(output) => println!("output: {}", output.output),
-        //Err(e) => println!("ererer: {}", e)
-    //}
+    println!("result: {}", result);
 }
